8da23917502fdf931a16e055cf1f62457e3686b7
eralonso <eric.alonso2003@gmail.com>
2025-04-21 19:35:15 +0200

    feat: Add macros to simplify the double pointer dereference and tests

--- src/main.asm
+++ src/main.asm
@@ -1,23 +1,74 @@
 %include "syscall/io.inc"
 %include "syscall/std.inc"
 %include "profile.inc"
+%include "funcall.inc"
 
 global _start 
 
-%assign SIZE 4
+; Move by dereference twice the first parameter
+%macro movdd1 3
+
+	push rax
+	mov rax, [%1]
+	mov %3 [rax], %2
+	pop rax
+
+%endmacro
+
+; Move by dereference twice the second parameter
+%macro movdd2 3
+
+	push rax
+	mov rax, [%2]
+	mov %1, %3 [rax]
+	pop rax
+
+%endmacro
+
+; Compare by dereference twice the first parameter
+%macro cmpdd1 3
+
+	push rax
+	mov rax, [%1]
+	cmp %3 [rax], %2
+	pop rax
+
+%endmacro
 
 section .text
 
-_start:
-	prologue SIZE
+print:
+	prologue 8
 
-	lea rbp, [rbp - SIZE]
+	mov [rbp - 8], rdi
+.loop_start:
 
-	read STDIN_FILENO, rbp, SIZE
-	write STDOUT_FILENO, rbp, SIZE
+	cmpdd1 rbp - 8, 0, byte
+	je .loop_end
 
-	lea rbp, [rbp + SIZE]
+	write STDOUT_FILENO, [rbp - 8], 1
 
+	inc byte [rbp - 8]
+	jmp .loop_start
+
+.loop_end:
 	epilogue
+	ret
+	
+
+introduction:
+
+	; write STDOUT_FILENO, msg, msg.len
+	funcall print, msg
+
+	ret
+
+_start:
+
+	funcall introduction
 
 	exit 0
+
+section .data
+msg: db `Starting Webserver...\n\0`
+	.len equ $ - msg
